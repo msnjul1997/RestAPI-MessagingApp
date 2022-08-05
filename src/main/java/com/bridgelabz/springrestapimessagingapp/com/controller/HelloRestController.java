66bbd1686e097ff6e21a7990c38eeca02e02a695
msnkittu859 <Vamshikalyan97>
2022-08-05 11:57:40 +0530

    Added: Using Get Req Method & Pass Name As Query Parameter

--- src/main/java/com/bridgelabz/springrestapimessagingapp/com/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/springrestapimessagingapp/com/controller/HelloRestController.java
@@ -3,17 +3,29 @@
  */
 package com.bridgelabz.springrestapimessagingapp.com.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class HelloRestController {
 
-
+    /**
+     * purpose : Worn the request mapping is done then all the statements  of sayHello gets executed
+     */
     @RequestMapping(value="/hello")
     public String sayHello(){
         return "Hello Form BridgeLabz";
     }
 
+    /**
+     * @RequestParam : Mapping HTTP GET Request onto specific Handler Methods.
+     *                 When mapping is done all statements of  sayHello method get executed
+     *  curl : localhost:8080/getMessage?name=Ashish
+     */
+    @GetMapping(value="/getMessage")
+    public String sayHello(@RequestParam(value = "name" )String name){
+        return "Hello " + name + "Welcome To BridgeLabz";
+    }
 }
